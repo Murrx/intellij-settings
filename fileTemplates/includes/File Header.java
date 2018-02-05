6dc1ea27890b4a40fb2318b56ce2327838b717af
Robin Altena <robin.altena@sogeti.com>
2018-02-05 16:13:06 +0100

    IU-2017.2.6 <robin@robin-ThinkPad-T550 Create vim_settings.xml, TempProfile.xml, IntelliLang.xml, ide.general.xml, Java8.xml, project.default.xml, diff.xml, equalsHashCodeTemplates.xml, Enum.java, Interface.java, other.xml, applicationLibraries.xml, JCN Style Guide 3_0.xml, Default.xml, file.template.settings.xml, ignore.xml, keymap.xml, find.xml, ui.lnf.xml, File Header.java, projectView.xml, Default for XWin copy.xml, javaeeExternalResources.xml, code.style.schemes.xml, colors.scheme.xml, External Tools.xml, git.xml, databaseDrivers.xml, templates.xml, AnnotationType.java, SQL.xml, scala.xml, parameter.hints.xml, Class.java, jdk.table.xml, laf.xml, debugger.renderers.xml, JCN checkstyle.xml, intentionSettings.xml, Default _1_.xml, vcs.xml, databaseSettings.xml, toStringTemplates.xml, editor.xml, usageView.xml, debugger.xml, path.macros.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,13 @@
+/*------------------------------------------------------------------------------
+ **     Ident: Delivery Center Java
+ **    Author: ${USER}
+ ** Copyright: (c) ${DAY} ${MONTH_NAME_SHORT}. ${YEAR} Sogeti Nederland B.V. All Rights Reserved.
+ **------------------------------------------------------------------------------
+ ** Sogeti Nederland B.V.            |  No part of this file may be reproduced  
+ ** Distributed Software Engineering |  or transmitted in any form or by any        
+ ** Lange Dreef 17                   |  means, electronic or mechanical, for the      
+ ** 4131 NJ Vianen                   |  purpose, without the express written    
+ ** The Netherlands                  |  permission of the copyright holder.
+ *------------------------------------------------------------------------------
+ */
+ 
